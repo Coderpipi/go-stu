18873d86bd7940ca8a12a7e82ea96da8fed08b7c
pipi <2582482991@qq.com>
2022-08-21 09:41:40 +0800

    feat: add new problem

--- leetcode/editor/cn/[219]存在重复元素 II.go
+++ leetcode/editor/cn/[219]存在重复元素 II.go
@@ -1,25 +1,25 @@
-//给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i 
-//- j) <= k 。如果存在，返回 true ；否则，返回 false 。 
+// 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i
+// - j) <= k 。如果存在，返回 true ；否则，返回 false 。
 //
 // 
 //
 // 示例 1： 
 //
 // 
-//输入：nums = [1,2,3,1], k = 3
-//输出：true 
+// 输入：nums = [1,2,3,1], k = 3
+// 输出：true
 //
 // 示例 2： 
 //
-// 
-//输入：nums = [1,0,1,1], k = 1
-//输出：true 
+// git
+// 输入：nums = [1,0,1,1], k = 1
+// 输出：true
 //
 // 示例 3： 
 //
 // 
-//输入：nums = [1,2,3,1,2,3], k = 2
-//输出：false 
+// 输入：nums = [1,2,3,1,2,3], k = 2
+// 输出：false
 //
 // 
 //
@@ -37,7 +37,7 @@ package cn
 
 import "math"
 
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 func containsNearbyDuplicate(nums []int, k int) bool {
 	n := len(nums)
 	if n < 2 {
@@ -56,4 +56,4 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 	return false
 }
 
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
